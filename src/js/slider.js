2d2485741be7ae5f7a86f7dc22961e9d0912d86b
Olena1010 <ebojcenko55@gmail.com>
2023-11-22 22:14:32 +0200

    add

---
+++ src/js/slider.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use([Navigation]);
+
+const swiper1 = new Swiper('.swiper-1', {
+  direction: 'horizontal',
+  // slidesPerView: 4,
+  loop: true,
+  // spaceBetween: 20,
+  slidesPerView: 2,
+//   spaceBetween: 20,
+  width: 100,
+
+  breakpoints: {
+    // when window width is >= 320px
+    // 320: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 20,
+    //   width: 295,
+    // },
+    // // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 2,
+    //   spaceBetween: 20,
+    //   width: 610,
+    // },
+    // when window width is >= 640px
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+      width: 925,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      width: 1240,
+    },
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next-1',
+    prevEl: '.swiper-button-prev-1',
+  },
+  speed: 600,
+});
+
+export { swiper1 };
